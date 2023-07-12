0a6520d9060eee5d8969682a02cca7d205067f06
Josh Thomas <bryanthxo@gmail.com>
2023-07-11 21:05:22 -0400

    Homepage and unit-prices pages where created with all its section

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// document.getElementById('unit').addEventListener('click', function() {
+
+//     document.getElementById('unit').classList.toggle('active');
+//     document.getElementById('home').classList.remove('active');
+//     document.getElementById('contact').classList.remove('active');
+
+// });
+
+// document.getElementById('home').addEventListener('click', function() {
+
+//     document.getElementById('home').classList.toggle('active');
+//     document.getElementById('unit').classList.remove('active');
+//     document.getElementById('contact').classList.remove('active');
+
+// });
+
+// document.getElementById('contact').addEventListener('click', function() {
+
+//     document.getElementById('contact').classList.toggle('active');
+//     document.getElementById('unit').classList.remove('active');
+//     document.getElementById('home').classList.remove('active');
+
+// });
